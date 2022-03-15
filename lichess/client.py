c6964be73e759f2034099372f65658bdb1e12f8c
Alejandro "qe" Ismodes <59273057+qe@users.noreply.github.com>
2022-03-15 06:05:12 -0500

    Added/cleaned methods and added enums
    - More methods for endpoints need to be implemented
    - README needs to be updated

--- lichess/client.py
+++ lichess/client.py
@@ -1,27 +1,39 @@
-
-
 # from .utils import *
+from .enums import *
 import requests
 import urllib
+import logging
 
 
-_ROOT_URL = "https://lichess.org/"
+logger = logging.getLogger(__name__)
+VALID_PERF_TYPES = [_.value for _ in PerfType]
 
 
 class Client:
-    def __init__(self):
-        self.url = _ROOT_URL
+    def __init__(self, token=None):
+        self.url = "https://lichess.org/"
+        self.s = requests.Session()  # keep session alive to improve performance <3
+        if token:
+            self.token = token
+            # self.s.headers.update(token)
 
-    def request(self, path, post_data=None, params=None, *args, **kwargs):
+    def request(self, path, oauth=False, post_data=None, *args, **kwargs):
         full_url = urllib.parse.urljoin(self.url, path)
+        print("hitting this URL:", full_url)
 
-        # GET method
-        if not post_data:
-            response = requests.get(full_url)
-        #
-        else:
-            print("POST method!!!")
-            return
+        try:
+            if oauth:
+                print("OAUTH status:", oauth)
+                response = self.s.get(full_url, headers={"Authorization": f"Bearer {self.token}"})
+            else:
+                response = self.s.get(full_url)
+            # res = requests.get(address, timeout=30)  # add timeout
+        except requests.exceptions.RequestException as err:
+            logger.error(err)
+            raise
+
+        # print(response.content)
+        # print(response.text)
 
         # print("response.status_code", response.status_code)
 
@@ -32,6 +44,7 @@ class Client:
         # if the response contains invalid JSON, attempting r.json() raises requests.exceptions.JSONDecodeError
         if response.status_code == 200:
             response2json = response.json()
+            print(type(response2json))
             # if isinstance(response2json, list):
             #     return response2json[0]
             return response2json
@@ -40,166 +53,230 @@ class Client:
             print("ERROR!", response.status_code)
             return
 
-    def get(self, path, params=None, *args, **kwargs):
-        # may need to take authorization and format into account
-        return self.request(path)
+    # -- Account --------------------------------------------------------------
 
-    # def post(self, path, post_data=None, params=None, *args, **kwargs):
+    def get_profile(self):
+        """Get your public profile information
+
+        :return: A dictionary with your public information
+        :rtype: dict
+        """
+        endpoint = "api/account"
+        return self.request(path=endpoint, oauth=True)
+
+    def get_email(self):
+        """Get your email address
+
+        :return: A dictionary with your email address
+        :rtype: dict
+        """
+        endpoint = "api/account/email"
+        return self.request(path=endpoint, oauth=True)
+
+    def get_preferences(self):
+        """Get your preferences
+
+        :return: A dictionary with your preferences
+        :rtype: dict
+        """
+        endpoint = "api/account/preferences"
+        return self.request(path=endpoint, oauth=True)
+
+    def get_kid_mode(self):
+        """Get your kid mode status
+
+        :return: A dictionary with your kid mode status
+        :rtype: dict
+        """
+        endpoint = "api/account/kid"
+        return self.request(path=endpoint, oauth=True)
+
+    """
+    Implement post ability in client.py
+    """
+    # def set_kid_mode(self):
+    #     """Set your kid mode status
+    #
+    #     :return:
+    #     :rtype:
+    #     """
+    #     endpoint = "api/account/kid"
+    #     # POST
+    #     pass
+
+    # -- Users ----------------------------------------------------------------
+
+    def get_status(self, *users, with_game_ids=False):
+        """Get real-time status of one or more users
+
+        :param users: User to query their real-time status
+        :type users: str
+        :param with_game_ids: Flag to know whether or not include the ID of games being played, if any, for each player
+        :type with_game_ids: bool, optional
+        :return: A list with a nested dictionary containing the real-time status of one or more users
+        :rtype: list
+        """
+        endpoint = "api/users/status"
+        path = endpoint + "?ids=" + ','.join(users)
+
+        if with_game_ids:
+            path += "&withGameIds=true"
+
+        return self.request(path=path)
+
+    """
+    Create function in utils.py to manually parse these two responses (JSON problems)
+    """
+    # def get_top_ten(self):
+    #     """Get the top 10 players for each speed and variant
+    #
+    #     :return:
+    #     """
+    #     endpoint = "player"
+    #     return self.request(path=endpoint)
+    #
+    # def get_leaderboard(self, perf_type, num_users):
+    #     """Get leaderboard of an individual speed or variant
+    #
+    #     :return:
+    #     """
+    #     if perf_type not in VALID_PERF_TYPES:
+    #         # throw error
+    #         raise Exception("Invalid perf_type value")
+    #
+    #     endpoint = "player/top/{nb}/{perfType}"
+    #     path = endpoint.format(nb=num_users, perfType=perf_type)
+    #     return self.request(path=path)
+
+    def get_data(self, user):
+        """Get public data of an individual user
+
+        :param user: User to query their public data
+        :type user: str
+        :return: A dictionary with the public data of the user
+        :rtype: dict
+        """
+        endpoint = "api/user/{username}"
+        path = endpoint.format(username=user)
+        return self.request(path=path)
+
+    def get_rating_history(self, user):
+        """Get rating history of an individual user
+
+        :param user: User to query their public data
+        :type user: str
+        :return: A list with a nested dictionary containing the rating history of the user
+        :rtype: list
+        """
+        endpoint = "api/user/{username}/rating-history"
+        path = endpoint.format(username=user)
+        return self.request(path=path)
+
+    """
+    Possibly add Enum for PerfType (also related to the get_leaderboard() method)
+    """
+    def get_stats(self, user, perf_type):
+        """Get performance statistics of an individual user
+
+        :param user: User to query their performance statistics
+        :type user: str
+        :param perf_type: Type of speed or variant to query
+        :type user: str
+        :return: A dictionary with the performance statistics of the user
+        :rtype: dict
+        """
+        if perf_type not in VALID_PERF_TYPES:
+            # throw error
+            raise Exception("Invalid perf_type value")
+
+        endpoint = "api/user/{username}/perf/{perf}"
+        path = endpoint.format(username=user, perf=perf_type)
+        return self.request(path=path)
+
+    def get_activity(self, user):
+        """Get the activity feed of an individual user
+
+        :param user: User to query their activity feed
+        :type user: str
+        :return: A list with a nested dictionary containing the activity feed of the user
+        :rtype: list
+        """
+        endpoint = "api/user/{username}/activity"
+        path = endpoint.format(username=user)
+        return self.request(path=path)
+
+    # def get_by_id():
+    #     """
+    #     Get users by ID
+    #
+    #     :return:
+    #     """
+    #     endpoint = "api/users"
+    #     pass
+    #
+    #
+    # def get_team_members():
+    #     """
+    #     Get members of a team
+    #
+    #     :return:
+    #     """
+    #     endpoint = "api/team/{teamId}/users"
+    #     pass
+    #
+    #
+    # def get_live_streamers():
+    #     """
+    #     Get live streamers
+    #
+    #     :return:
+    #     """
+    #     endpoint = "streamer/live"
+    #     pass
+    #
+    #
+    # def get_crosstable():
+    #     """
+    #     Get crosstable
+    #
+    #     :return:
+    #     """
+    #     endpoint = "api/crosstable/{user1}/{user2}"
     #     pass
 
 
-client = Client()
+    # # CUSTOM
+    # def get_followers():
+    #     endpoint =
+    #     pass
+    #
+    # # CUSTOM
+    # def get_following():
+    #     endpoint =
+    #     pass
 
+    # -- Relations ------------------------------------------------------------
 
-# -- Users ----------------------------------------------------------------
+    def relations(self):
+        endpoint = "api/rel/following"
+        return self.request(path=endpoint, oauth=True)
 
-def get_status(*users, with_game_ids=False):
-    """
-    Get real-time users status
-
-    :return:
-    """
-    endpoint = "api/users/status"
-    path = endpoint + "?ids=" + ','.join(users)
-
-    if with_game_ids:
-        path += "&withGameIds=true"
-
-    return client.get(path=path)
-
-
-def get_top_ten():
-    """
-    Get all top 10
-
-    :return:
-    """
-    endpoint = "player"
-    pass
-
-
-def get_leaderboard():
-    """
-    Get one leaderboard
-
-    :return:
-    """
-    endpoint = "player/top/{nb}/{perfType}"
-    pass
-
-
-def get_data(username):
-    """
-    Get user public data
-
-    :param username:
-    :return:
-    """
-    endpoint = "api/user/{username}"
-    path = endpoint.format(username=username)
-    # print(path)
-    return client.get(path=path)
-
-
-def get_rating_history():
-    """
-    Get rating history of a user
-
-    :return:
-    """
-    endpoint = "api/user/{username}/rating-history"
-    pass
-
-
-def get_stats():
-    """
-    Get performance statistics of a user
-
-    :return:
-    """
-    endpoint = "api/user/{username}/perf/{perf}"
-    pass
-
-
-def get_activity():
-    """
-    Get user activity
-
-    :return:
-    """
-    endpoint = "api/user/{username}/activity"
-    pass
-
-
-def get_by_id():
-    """
-    Get users by ID
-
-    :return:
-    """
-    endpoint = "api/users"
-    pass
-
-
-def get_team_members():
-    """
-    Get members of a team
-
-    :return:
-    """
-    endpoint = "api/team/{teamId}/users"
-    pass
-
-
-def get_live_streamers():
-    """
-    Get live streamers
-
-    :return:
-    """
-    endpoint = "streamer/live"
-    pass
-
-
-def get_crosstable():
-    """
-    Get crosstable
-
-    :return:
-    """
-    endpoint = "api/crosstable/{user1}/{user2}"
-    pass
-
-
-# # CUSTOM
-# def get_followers():
-#     endpoint =
-#     pass
-#
-# # CUSTOM
-# def get_following():
-#     endpoint =
-#     pass
-
-
-# -- Account --------------------------------------------------------------
-# -- Relations ------------------------------------------------------------
-# -- Games ----------------------------------------------------------------
-# -- TV -------------------------------------------------------------------
-# -- Puzzles --------------------------------------------------------------
-# -- Teams ----------------------------------------------------------------
-# -- Board ----------------------------------------------------------------
-# -- Bot ------------------------------------------------------------------
-# -- Challenges -----------------------------------------------------------
-# -- Bulk pairings --------------------------------------------------------
-# -- Arena tournaments ----------------------------------------------------
-# -- Swiss Tournaments ----------------------------------------------------
-# -- Simuls ---------------------------------------------------------------
-# -- Studies --------------------------------------------------------------
-# -- Messaging ------------------------------------------------------------
-# -- Broadcasts -----------------------------------------------------------
-# -- Analysis -------------------------------------------------------------
-# -- Opening Explorer -----------------------------------------------------
-# -- Tablebase ------------------------------------------------------------
-# -- OAuth ----------------------------------------------------------------
+    # -- Games ----------------------------------------------------------------
+    # -- TV -------------------------------------------------------------------
+    # -- Puzzles --------------------------------------------------------------
+    # -- Teams ----------------------------------------------------------------
+    # -- Board ----------------------------------------------------------------
+    # -- Bot ------------------------------------------------------------------
+    # -- Challenges -----------------------------------------------------------
+    # -- Bulk pairings --------------------------------------------------------
+    # -- Arena tournaments ----------------------------------------------------
+    # -- Swiss Tournaments ----------------------------------------------------
+    # -- Simuls ---------------------------------------------------------------
+    # -- Studies --------------------------------------------------------------
+    # -- Messaging ------------------------------------------------------------
+    # -- Broadcasts -----------------------------------------------------------
+    # -- Analysis -------------------------------------------------------------
+    # -- Opening Explorer -----------------------------------------------------
+    # -- Tablebase ------------------------------------------------------------
+    # -- OAuth ----------------------------------------------------------------
 
