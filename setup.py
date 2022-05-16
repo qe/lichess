ab1f08cbd08bdb1fe02880323e76fefc99c8f761
Alejandro "qe" Ismodes <59273057+qe@users.noreply.github.com>
2022-05-16 15:44:11 -0400

    Bumping version number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.rst") as f:
 
 setup(
     name="lichess",
-    version="0.2.5",
+    version="0.2.6",
     description="Python wrapper for the Lichess API",
     long_description=long_description,
     long_description_content_type="text/x-rst",
